c45160a4bd2b1ed3753d40496f5249b778449dc2
kumarc21 <Chandankumar489@gmail.com>
2023-02-22 15:33:52 +0530

    multiple environment support for flutter

---
+++ android/app/src/main/kotlin/com/example/environment_command_poc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.environment_command_poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ scripts/configure_project_android.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f "../../.env.$1" ]; then
+
+  export $(cat "../../.env.$1" | sed 's/#.*//g' | xargs)
+
+fi
+
+echo "$ENCODED_FILE_KEY" | base64 --decode > "filename.txt"
